f2756fb239d8af7caf3cb3e1e48073753d5acfbe
p-montagnana <paula-eco@hotmail.com>
2024-06-26 17:17:38 -0300

    commit 4

--- script_cria_post_projetc.R
+++ script_cria_post_projetc.R
@@ -1,6 +1,6 @@
 # código para novo post, novo projeto...
 # já cria a pasta
 
-blogdown::new_post(title = "Euglossini PELD",
+blogdown::new_post(title = "Omdena",
                    ext = '.Rmarkdown',
                    subdir = "project")
